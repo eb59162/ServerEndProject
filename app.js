5047ee1ace8cb729c4aaf46948a242f7e277c94e
eb59162 <stb1641@gmail.com>
2024-07-14 19:56:46 +0300

    async crud

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ app.post('/login', (req, res) => {
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
-PORT = process.env.PORT || 8000
-CONNECTION = process.env.CONNECTION || "mongodb://127.0.0.1:27017/stories"
+
 
 mongoose.connect(CONNECTION).then(
     ()=>app.listen(PORT,()=>console.log(`server runing on port ${PORT}`)))
